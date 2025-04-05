95886a6d0c91a69099c3c7b86cb85cf2a76b8959
Jian Z'car Esteban <esteban.jianzcar@outlook.com>
2025-04-05 20:07:17 +0800

    Update portainerapache.rb

--- Formula/portainerapache.rb
+++ Formula/portainerapache.rb
@@ -7,6 +7,7 @@ class Portainerapache < Formula
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "8beaaa55379fb72b3e0900205fb8e4cb30aba4de237965f65cdb868532139c17"
+    url "https://github.com/JianZcar/PortainerApache/releases/download/1.00.01/portainerapache.tar.gz"
   end
   
   livecheck do
