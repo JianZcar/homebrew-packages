b7e0caa9297bb246dfccf2bb4181bb447f1e2391
Jian Z'car Esteban <esteban.jianzcar@outlook.com>
2025-04-05 20:28:58 +0800

    Update fuzpad.rb

--- Formula/fuzpad.rb
+++ Formula/fuzpad.rb
@@ -5,19 +5,10 @@ class Fuzpad < Formula
   sha256 "bd5b458dd118ad4a679a765817a826089c5d1ac7247637db329d698fe5747b42"
   license "GPL-3.0-or-later"
 
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
-
   depends_on "bat"
   depends_on "fzf"
 
   def install
-    bin.install "fuzpad"
-  end
-
-  test do
-    assert_equal "1", shell_output("#{bin}/fuzpad --test 2>&1 | grep -q \"1\" && echo 1 || echo 0").strip
+    bin.install "bin/fuzpad" => "fuzpad"
   end
 end
