f7e67c921eea63bd52795b0c35b71d582fe81f88
Jian Z'car Esteban <esteban.jianzcar@outlook.com>
2025-04-05 20:13:10 +0800

    Update portainerapache.rb

--- Formula/portainerapache.rb
+++ Formula/portainerapache.rb
@@ -5,20 +5,10 @@ class Portainerapache < Formula
   sha256 "8beaaa55379fb72b3e0900205fb8e4cb30aba4de237965f65cdb868532139c17"
   license "GPL-3.0-or-later"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "8beaaa55379fb72b3e0900205fb8e4cb30aba4de237965f65cdb868532139c17"
-    url "https://github.com/JianZcar/PortainerApache/releases/download/1.00.01/portainerapache.tar.gz"
-  end
-  
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
-
   depends_on "tree"
   depends_on "fzf"
 
   def install
-    bin.install "bin/portainer-apache"
+    bin.install "bin/portainer-apache" => "portainer-apache"
   end
 end
